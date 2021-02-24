2876049c048dde42c2342985530a15d681ae8e3c
milypm <mp_morones@hotmail.com>
2021-02-24 02:01:30 -0600

    Fix linters

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -5,10 +5,10 @@ module EventsHelper
 
   def attend_btn
     attend_btn = button_to 'Attend Event', attend_event_path(@event.id),
-                          params: { 
-                            attended_event_id: @event.id,
-                            event_attendee_id: current_user.id
-                          }, class: 'btn btn-success'
+                           params: {
+                             attended_event_id: @event.id,
+                             event_attendee_id: current_user.id
+                           }, class: 'btn btn-success'
     if EventAttending.find_by(attended_event_id: @event.id).nil?
       attend_btn
     elsif EventAttending.exists?(attended_event_id: @event.id, event_attendee_id: current_user.id)
@@ -22,9 +22,9 @@ module EventsHelper
     return unless EventAttending.exists?(attended_event_id: @event.id, event_attendee_id: current_user.id)
 
     button_to 'Unattend Event', cancel_event_attending_path(@event.id),
-              params: { 
-                attended_event_id: @event.id,
-                event_attendee_id: current_user.id 
-              }, method: :delete, class: 'btn btn-outline-secondary btn-sm mt-2'
+               params: {
+                 attended_event_id: @event.id,
+                 event_attendee_id: current_user.id
+               }, method: :delete, class: 'btn btn-outline-secondary btn-sm mt-2'
   end
 end
